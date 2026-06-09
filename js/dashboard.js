24fdf17c80b18acc98c4cf113b32d73754327aae
Divyanshu <divyanshu.4987@gmail.com>
2026-06-09 16:29:51 +0530

    Added dynamic habit creation and progress updates

--- js/dashboard.js
+++ js/dashboard.js
@@ -1,7 +1,7 @@
 const checkboxes = document.querySelectorAll('input[type="checkbox"]'); //find all the checkboxes
 const progressText = document.querySelector("#progress-text"); //find the element with id = progress-text
-
 function updateProgress(){
+    const checkboxes = document.querySelectorAll('input[type="checkbox"]'); //find all the checkboxes
     let completed = 0;
     checkboxes.forEach(function(checkbox){//for each checkbox, check whether they're checked or not
         if(checkbox.checked){
@@ -15,5 +15,31 @@ checkboxes.forEach(function(checkbox){//for all of the checkboxes, if the state 
     checkbox.addEventListener("change", updateProgress);
 
 });
-
 updateProgress();
+
+//for adding habits
+const habitInput = document.querySelector("#habit-input");
+const addHabitBtn = document.querySelector("#add-habit-btn");
+const habitsContainer = document.querySelector(".habits");
+
+//function for adding habits
+addHabitBtn.addEventListener("click", function(){
+    const habitText = habitInput.value;//take user input
+    if(habitText != ""){
+        const newHabit = document.createElement("div");//create a div 
+        newHabit.classList.add("habit-item");// put class on that div
+        //providing the inner HTML of div with class = habit-item
+        newHabit.innerHTML = `
+        <input type="checkbox">
+        <label>${habitText}</label>
+        `;
+        habitsContainer.appendChild(newHabit);//add this newhabit(div) into habits container
+        const newCheckbox = newHabit.querySelector("input"); //find the input in div we created above
+        newCheckbox.addEventListener("change", updateProgress); //if the state of input of div we selected changes, run the function
+        updateProgress(); //if add habit button is clicked, run the function to recalculate the progress
+        habitInput.value = ""; // this is used to remove the habit still written inside the input box after clicking add button
+    }
+    else{
+        alert("Please enter a habit");
+    }
+} );
