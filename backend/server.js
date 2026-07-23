1dd34c93df7f69fd195049a283dac1f6e2b6934c
Divyanshu <divyanshu.4987@gmail.com>
2026-07-23 11:21:36 +0530

    Configure backend for production deployment

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ app.use("/api", authRoutes);
 app.use("/api", aiRoutes);
 
 //basically browser's address
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 //route
 app.get("/", (req, res) => {
